fd5a08bec6074995a0db96c0c8aa6441f906c8f5
Joshua Thomas <90353177+JoshuasProgramming@users.noreply.github.com>
2023-01-23 17:42:05 +0000

    added & commented code for haskell

--- LearningHaskell_Exercises-master/dataTypes.hs
+++ LearningHaskell_Exercises-master/dataTypes.hs
@@ -9,3 +9,8 @@ multi = 5 * 5
 -- We'll take in two integers and output a integer
 combine x y = x + y
 
+
+--making a factorial function
+
+fact 0 = 1 -- if statement to state that if 0 is inputted we'll output 1
+fact n = n * fact (n - 1) -- n equals (n * fact(n-1 * fact(n - 1))...)
